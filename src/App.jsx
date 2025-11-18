05fe7dabd2a5c60fd8f8dcca588fa9aa559bf00e
Samuel Olabode <Solabode499@gmail.com>
2025-11-17 18:18:34 -0700

    `Added multiple properties to the properties array in App.jsx and updated the Properties component to handle optional price values`

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ const properties  = [
   {id: 3, name : 'Cactus Retreat', rating : 4, price : 500},
   {id: 4, name : 'Ocean View Condo', rating : 7, price : 450},
   {id: 5, name : 'Gold Miner Campground', rating : 8, price : 550},
+  {id: 6, name : 'Gold Miner Campground', rating : 8, price : 55},
+  {id: 7, name : 'Gold Miner Campground', rating : 8, price : 99},
+  {id: 8, name : 'Gold Miner Campground', rating : 8, price : 340},
+  {id: 9, name : 'Gold Miner Campground', rating : 8, price : 400},
+  {id: 10, name : 'Gold Miner Campground', rating : 8, price : 19},
+  {id: 11, name : 'Gold Miner Campground', rating : 8, price : 120},
+  {id: 12, name : 'Gold Miner Campground', rating : 8, price : 1500},
+  {id: 13, name : 'Gold Miner Campground', rating : 8, price : 780},
+  {id: 14, name : 'Gold Miner Campground', rating : 8},
+  {id: 15, name : 'Gold Miner Campground', rating : 8, price : 512},
+  {id: 16, name : 'Gold Miner Campground', rating : 8, price : 230},
 ];
 
 function App() {
--- src/Properties.jsx
+++ src/Properties.jsx
@@ -3,7 +3,7 @@ function Properties ({name, rating, price}){
         <li className="property-item">
             <div><strong>{name}</strong></div>
             <div>{price ? 'Available' : 'Not Available'}</div>
-            <div>Price : {price}</div>
+            <div>{price && price }</div>
             <div>Rating: {rating}/10</div>
         </li>
     )
